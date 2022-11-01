15530997aebbcb42b71ce3ef45617fba56fe0318
Furkan Cemal Çalışkan <furkanc.caliskan@icloud.com>
2022-11-01 23:21:12 +0300

    Updated Locations Section

---
+++ africa/Model/CoverImageModel.swift
@@ -0,0 +1,14 @@
+//
+//  CoverImageModel.swift
+//  africa
+//
+//  Created by Furkan Cemal Çalışkan on 1.11.2022.
+//
+
+import SwiftUI
+
+struct CoverImage : Codable, Identifiable {
+    let id : Int
+    let name : String
+}
+
