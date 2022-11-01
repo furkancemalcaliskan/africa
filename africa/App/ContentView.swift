6cc6df15ad6859df6fbc2adfd1166ff26c80ffdc
Furkan Cemal Çalışkan <furkanc.caliskan@icloud.com>
2022-11-01 16:21:28 +0300

    Parsed Data from Local JSON File

--- africa/App/ContentView.swift
+++ africa/App/ContentView.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Content")
+        NavigationView {
+            List {
+                CoverImageView()
+                    .frame(height: 300)
+                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+            }//: LIST
+            .navigationBarTitle("africa",displayMode: .large)
+        }//: NAVIGATION
     }
 }
 
---
+++ africa/View/CoverImageView.swift
@@ -0,0 +1,36 @@
+//
+//  CoverImageView.swift
+//  africa
+//
+//  Created by Furkan Cemal Çalışkan on 1.11.2022.
+//
+
+import SwiftUI
+
+struct CoverImageView: View {
+    //MARK: - PROPERTIES
+    
+    let coverImages : [CoverImage] = Bundle.main.decode("covers.json")
+    
+    //MARK: - BODY
+    
+    var body: some View {
+        TabView {
+            ForEach(coverImages) { item in
+                Image(item.name)
+                    .resizable()
+                .scaledToFill()
+            }//: LOOP
+        }//: TAB
+        .tabViewStyle(PageTabViewStyle())
+    }
+}
+
+//MARK: - PREVIEW
+
+struct CoverImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoverImageView()
+            .previewLayout(.fixed(width: 400, height: 300))
+    }
+}
