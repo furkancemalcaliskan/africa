ca21123e05100f7ff8b65218a6138610135b3878
Furkan Cemal Çalışkan <furkanc.caliskan@icloud.com>
2022-11-02 15:58:23 +0300

    Added Motion Animation to Gallery Section

--- africa/App/GalleryView.swift
+++ africa/App/GalleryView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct GalleryView: View {
     var body: some View {
-        Text("Gallery")
+        ScrollView(.vertical, showsIndicators: false) {
+            Text("Gallery")
+        }//: SCROLL
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(MotionAnimationView())
     }
 }
 
